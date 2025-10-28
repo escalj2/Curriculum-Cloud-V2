7a889b9f4f8084cace0ca903ceaef0f71e81cbf7
escalj2 <escalj2@gmail.com>
2025-10-27 19:25:04 -0700

    Updated LessonPlan class to include more necessary fields

--- LessonPlan.java
+++ LessonPlan.java
@@ -2,6 +2,12 @@ public class LessonPlan {
     
     int ID = 0;
     String Name = "";
+    int GradeLvl = 0;
     String[] Standards = {};
+    String[] Objectives = {};
+    String Summary = "";
+    String[] Materials = {};
     String[] Tags = {};
+    String InstructionalSteps = "";
+    String ExtensionActivities = "";
 }
